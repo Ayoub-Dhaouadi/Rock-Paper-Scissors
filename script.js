415e8745913764f76f640899624e6821dc73b6a9
Ayoub <ayoubdhaouadi08@gmail.com>
2022-11-14 11:38:35 +0100

    Finished html/css and initial js query selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+let playerScore = 0 
+let compScore = 0 
+
+
+let rockBtn = document.querySelector(".rock")
+let paperBtn = document.querySelector(".paper")
+let scissorsBtn = document.querySelector(".scissors")
+let playerOptions = [rockBtn,paperBtn,scissorsBtn]
+let compOptions = ["rock","paper","scissors"]
+
